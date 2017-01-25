6318b7c26a72f48ac21674bfb31faa8d5fe8e892
zhaoxin89 <105205016@qq.com>
2017-01-25 21:59:09 +0800

    add struct girl

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+struct girl
+{
+private:
+	int realAge = 28;
+public:
+	int getAge(){ return 20; }
+};
+
 int main(void)
 {
 	cout << "This is a test." << endl;
